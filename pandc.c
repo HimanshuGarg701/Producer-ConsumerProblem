9b0c827bd9b713e4ffeb772762c1a93d8449bef7
Himanshu Garg <hgarg701@gmail.com>
2020-07-07 21:54:41 -0700

    Initialized the assignment

--- pandc.c
+++ pandc.c
@@ -1,3 +1,5 @@
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
 #include <string.h>
 #include <pthread.h>
@@ -35,5 +37,20 @@ void put_item(int item)
 
 int main(int argc. char* argv[]) 
 {
+    struct timespec startTime;
+    struct timespec endTime;
+    time_t seconds;
+    long nanoSeconds;
+
+    startTime = clock_gettime(CLOCK_REALTIME, &start_time);
+    endTime = clock_gettime(CLOCK_REALTIME, &end_time);
+
+    seconds = end_time.tv_sec - start_time.tv_sec;
+    nano_seconds = end_time.tv_nsec - start_time.tv_nsec;
+    
+    if(end_time.tv_nsec < start_time.tv_nsec){
+        seconds--;
+        nano_seconds = nano_seconds + 1000000000L;
+    }
 
 }
