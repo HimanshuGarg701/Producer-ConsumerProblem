3a0826dc12e15edbc7530d778717e405776d9848
Himanshu Garg <hgarg701@gmail.com>
2020-07-11 18:29:15 -0700

    Stored user input

--- pandc.c
+++ pandc.c
@@ -11,6 +11,14 @@
 
 
 
+int N;      //Number of Buffers
+int P;      //Number of Producer Threads
+int C;      //Number of consumer Threads
+int X;      //Products produced by each producer
+int Ptime;  //Sleep time for producer thread after producing 1 product
+int Ctime;  //Sleep time for consumer thread after consuming 1 product
+
+
 /**
  * for the 2 functions below, look
  * at the process slides/video for 
@@ -35,22 +43,44 @@ void put_item(int item)
     
 }
 
-int main(int argc. char* argv[]) 
+int main(int argc, char* argv[]) 
 {
-    struct timespec startTime;
-    struct timespec endTime;
-    time_t seconds;
-    long nanoSeconds;
 
-    startTime = clock_gettime(CLOCK_REALTIME, &start_time);
-    endTime = clock_gettime(CLOCK_REALTIME, &end_time);
+    if(argc!=7){
+        printf("\nMust have 6 arguments\n");
+        exit(0);
+    }
+    else{
+        N = atoi(argv[1]);
+        P = atoi(argv[2]);
+        C = atoi(argv[3]);
+        X = atoi(argv[4]);
+        Ptime = atoi(argv[5]);
+        Ctime = atoi(argv[6]);
 
-    seconds = end_time.tv_sec - start_time.tv_sec;
-    nano_seconds = end_time.tv_nsec - start_time.tv_nsec;
-    
-    if(end_time.tv_nsec < start_time.tv_nsec){
-        seconds--;
-        nano_seconds = nano_seconds + 1000000000L;
+        printf("\nValues received are\n");
+        printf("N is: %d\n", N);
+        printf("P is: %d\n", P);
+        printf("C is: %d\n", C);
+        printf("X is: %d\n", X);
+        printf("Ptime is: %d\n", Ptime);
+        printf("Ctime is: %d\n", Ctime);
+
+        struct timespec startTime;
+        struct timespec endTime;
+        time_t seconds;
+        long nanoSeconds;
+
+        // clock_gettime(CLOCK_REALTIME, &startTime);
+        // clock_gettime(CLOCK_REALTIME, &endTime);
+
+        // seconds = endTime.tv_sec - startTime.tv_sec;
+        // nanoSeconds = endTime.tv_nsec - startTime.tv_nsec;
+        
+        // if(endTime.tv_nsec < startTime.tv_nsec){
+        //     seconds--;
+        //     nanoSeconds = nanoSeconds + 1000000000L;
+        // }
     }
 
 }
